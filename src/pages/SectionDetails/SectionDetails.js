17912e27f569a16c32653947632b1a4c3e1e0c84
Filip Filipovic <filipfilipovic.stray@gmail.com>
2020-04-27 16:18:09 +0200

    implemented custom spinner

--- src/pages/SectionDetails/SectionDetails.js
+++ src/pages/SectionDetails/SectionDetails.js
@@ -4,20 +4,24 @@ import { Container, Row, Col } from "react-bootstrap";
 
 import NewsCard from "../../components/NewsCard/NewsCard";
 import SectionHeader from "../../components/SectionHeader/SectionHeader";
+import Spinner from "../../components/Spinner/Spinner";
 
 import { API_KEY } from "../../constants";
 
 const SectionDetails = (props) => {
   const { section } = useParams();
   const [news, setNews] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchSections = async (section) => {
+      setIsLoading(true);
       const res = await fetch(
         `https://content.guardianapis.com/${section}?show-fields=headline,trailText,body,thumbnail&page-size=20&api-key=${API_KEY}`
       );
       const json = await res.json();
       setNews(json.response.results);
+      setIsLoading(false);
     };
 
     fetchSections(section);
@@ -26,14 +30,18 @@ const SectionDetails = (props) => {
   return (
     <Container>
       <SectionHeader> {section}</SectionHeader>
-      <Row>
-        {news &&
-          news.map((item) => (
-            <Col key={item.id}>
-              <NewsCard article={item} />
-            </Col>
-          ))}
-      </Row>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <Row>
+          {news &&
+            news.map((item) => (
+              <Col key={item.id}>
+                <NewsCard article={item} />
+              </Col>
+            ))}
+        </Row>
+      )}
     </Container>
   );
 };
